ec099a8133a633d2b5c9383e57d94d213d6680a7
chuppyy <takhanhthient3@GMAIL.COM>
2024-02-16 20:42:00 +0700

    kk

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["apinews.sportsandtravelonline.com","api.sportsandtravelonline.com"],
+  },
+};
+
+export default nextConfig;
---
+++ src/pages/[slug].tsx
@@ -0,0 +1,79 @@
+import { Suspense } from "react";
+import { GetServerSideProps } from "next";
+import Script from "next/script";
+import axios from "axios";
+import Head from "next/head";
+const formatDate = (str: string) => {
+  const date = new Date(str);
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+};
+
+export default function Page(data: any) {
+  const article = data.data;
+  return (
+    <>
+      <Head>
+        <title>{article.name}</title>
+        <meta property="og:image" content={article.avatarLink} />
+        <meta property="og:title" content={article.name} />
+      </Head>
+      <Script id="gg-1" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-Y23MD9WKC2`} />
+      <Script id="gg-2" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-Y23MD9WKC2');
+        `}
+      </Script>
+      <main>
+        <Script src="/qcscript.js" />
+        <div className="container-flu details">
+                  <div id="M936536ScriptRootC1578537"></div>
+          <script
+                      src="https://jsc.adskeeper.com/l/i/lifenews.thongtinluat.com.1578537.js"
+            async
+          ></script>
+
+<div id="M936536ScriptRootC1578545"></div>
+<script src="https://jsc.adskeeper.com/l/i/lifenews.thongtinluat.com.1578545.js" async>
+</script>
+          <h1>{article.name}</h1>
+          <p className="mb-4 text-lg">
+            Posted: {formatDate(article.dateTimeStart)}
+          </p>
+          <Suspense fallback={<p>Loading ...</p>}>
+            <article
+              className="content"
+              dangerouslySetInnerHTML={{ __html: article.content }}
+            />
+          </Suspense>
+        </div>
+              <div id="M936536ScriptRootC1578542"></div>
+        <script
+                  src="https://jsc.adskeeper.com/l/i/lifenews.thongtinluat.com.1578542.js"
+          async
+        ></script>
+      </main>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<any> = async ({
+  params,
+}) => {
+    try {
+        
+    const response = await axios.get(
+        `${process.env.APP_API}/News/news-detail?id=${params?.slug?.slice(params?.slug?.lastIndexOf("-") + 1) }`
+    );
+    return {
+      props: { data: response.data.data },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: { data: [] as any[] }, // Sử dụng any type cho data
+    };
+  }
+};
